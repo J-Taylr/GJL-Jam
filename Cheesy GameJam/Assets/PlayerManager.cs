d2dccb1e49ed993e2f0141cc474f66b04d21d786
J-Taylor <jmarcustaylor@gmail.com>
2021-11-15 12:53:59 +0000

    player base movement

---
+++ Cheesy GameJam/Assets/PlayerManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int health = 3;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
