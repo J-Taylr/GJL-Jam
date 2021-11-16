f605b3888076abca026601633d9eb645fd6a8dc4
SR198889 <SR198889@falmouth.ac.uk>
2021-11-16 14:34:43 +0000

    added a patrol state and funcitioning AI to the CheeseGangsta prefab. if Bool chasing is false AI will patrol and if its true AI will Chase player

--- Cheesy GameJam/Assets/Scripts/Enemy/EnemyManager.cs
+++ Cheesy GameJam/Assets/Scripts/Enemy/EnemyManager.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
 
-    PlayerManager pM;
 
     public int health = 1;
 
+    public float patrolSpeed = 1.0f;
     public float speed = 1.0f;
     public float RotationSpeed;
     public float knockbackStrength = 50f;
+    public float startWaitTime;
 
     public bool Chasing = false;
 
     public Vector2 target;
 
+    public Transform[] moveSpots;
+
+    private int randomSpots;
+
+    private float waitTime;
+
     private Quaternion Q;
+
+
     // Start is called before the first frame update
     void Start()
     {
-     
+        waitTime = startWaitTime;
+        randomSpots = Random.Range(0, moveSpots.Length);
     }
 
     // Update is called once per frame
@@ -33,19 +43,11 @@ public class EnemyManager : MonoBehaviour
         }
         if (Chasing == true)
         {
-            // move towards player
-            GameObject player = GameObject.Find("Player");
-            target = player.transform.position;
-            Vector2 pos = gameObject.transform.position;
-
-            float step = speed * Time.deltaTime;
-
-            transform.position = Vector2.MoveTowards(transform.position, target, step);
-
-            // rotate towards player
-            Vector3 dir = player.transform.position - transform.position;
-            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            ChasingPlayer();
+        }
+        if (Chasing == false)
+        {
+            Patrol();
         }
 
     }
@@ -54,13 +56,11 @@ public class EnemyManager : MonoBehaviour
         
         if(collision.gameObject.tag == "Player")
         {
-            pM = collision.gameObject.GetComponent<PlayerManager>();
-
+            PlayerManager pM = collision.gameObject.GetComponent<PlayerManager>();
             pM.health--;
             Debug.Log("You did one damage");
 
             Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
-
             if(rb != null)
             {
                 Vector2 direction = collision.transform.position - transform.position;
@@ -71,6 +71,39 @@ public class EnemyManager : MonoBehaviour
                 Debug.Log("Knockback");
             }
 
+        }
+    }
+    void ChasingPlayer()
+    {
+        // move towards player
+        GameObject player = GameObject.Find("Player");
+        target = player.transform.position;
+        Vector2 pos = gameObject.transform.position;
+
+        float step = speed * Time.deltaTime;
+
+        transform.position = Vector2.MoveTowards(transform.position, target, step);
+
+        // rotate towards player
+        Vector3 dir = player.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+    void Patrol()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpots].position, patrolSpeed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveSpots[randomSpots].position) < 0.2f)
+        {
+            if(waitTime <= 0)
+            {
+                randomSpots = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
         }
     }
 }
